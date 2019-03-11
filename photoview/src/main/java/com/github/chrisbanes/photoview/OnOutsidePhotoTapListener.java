a029d8b519da7f0439f9e137441bad937c034b8a
buck <coderbuck@163.com>
2019-03-11 12:32:35 +0800

    note OnOutsidePhotoTapListener

--- photoview/src/main/java/com/github/chrisbanes/photoview/OnOutsidePhotoTapListener.java
+++ photoview/src/main/java/com/github/chrisbanes/photoview/OnOutsidePhotoTapListener.java
@@ -3,6 +3,7 @@ package com.github.chrisbanes.photoview;
 import android.widget.ImageView;
 
 /**
+ * 点击图片外部区域时回调
  * Callback when the user tapped outside of the photo
  */
 public interface OnOutsidePhotoTapListener {
